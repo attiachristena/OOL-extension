5833fbd3634c92f9081a57a9da14b57daaa0ba29
chri2193 <123303332+chri2193@users.noreply.github.com>
2023-12-20 16:23:22 +0100

    Update ProgettoProlog.pl

--- ProgettoProlog.pl
+++ ProgettoProlog.pl
@@ -1,6 +1,6 @@
 %%% -*- Mode: Prolog -*-
-%%%oop.pl
-%%%Attia Christena Mat 894887
+%%% oop.pl
+%%% Attia Christena Mat 894887
 
 % Predicati dinamici per definire le classi.
 :- dynamic class/2.
@@ -15,46 +15,81 @@
 % Predicato def_class per definire la struttura di una classe in OOΠ.
 
 % Definizione della classe senza parti (field o method).
-def_class(Class, Parents) :-
-atom(Class), 
-is_list(Parents),
+def_class(Class_Name, Parents) :-
+   atom(Class_Name), 
+   is_list(Parents),
 % Verifica che tutti gli elementi di Parents siano atomi.
-all_atoms(Parents),
+   all_atoms(Parents),
 % Memorizza la classe nella base di conoscenza di Prolog
-assertz(class(Class, Parents)).
+   assertz(class(Class_Name, Parents)).
 
 % Definizione della classe con parti specificate (field o method).
-def_class(Class, Parents, Parts) :-
-atom(Class), 
-is_list(Parents), 
+def_class(Class_Name, Parents, Parts) :-
+   atom(Class_Name),
+   is_list(Parents),
 % Verifica che tutti gli elementi di Parents siano atomi.
-all_atoms(Parents),
+   all_atoms(Parents),
 % Si assicura che Parts sia una lista valida di parti (field o method).
-process_parts(Class, Parts),
-% Memorizza la classe nella base di conoscenza di Prolog con le parti specificate.
-assertz(class(Class, Parents, Parts)).
-
-
-% process_parts: Processa le parti di una classe (fields o methods)
-process_parts(_, []).
-process_parts(Class, [Part|Rest]) :-
-    process_part(Class, Part),
-    process_parts(Class, Rest).
-
-% process_part: Processa una singola parte di una classe (field o method)
-process_part(Class, field(FieldName, Value)) :-
-    assertz(field(Class, FieldName, Value, _Type)).  % Assume un tipo non specificato per il campo.
-process_part(Class, field(FieldName, Value, Type)) :-
-    assertz(field(Class, FieldName, Value, Type)).
-process_part(Class, method(MethodName, ArgList, Form)) :-
-    assertz(method(Class, MethodName, ArgList, Form)).
-
+   valid_parts(Parts),
+% Memorizza la classe nella base di conoscenza di Prolog.
+   assertz(class(Class_Name, Parents, Parts)).
 
 % Verifica che tutti gli elementi di una lista siano atomi.
 all_atoms([]).
 all_atoms([H | T]) :-
    atom(H),
    all_atoms(T)
+
+% Controlla Parts e analizza il caso in cui sia un campo o un metodo.
+valid_parts([]).
+valid_parts([Part|Rest]) :-
+    (valid_field(Part);
+     valid_method(Part)),
+    valid_parts(Rest).
+
+% Validazione di un campo con due argomenti
+valid_field(field(FieldName, Value)) :-
+    atom(FieldName),
+    ground(Value), % Verifica se il valore è istanziato
+    !. % Utilizzo del cut per evitare backtracking indesiderato
+
+% Validazione di un campo con tre argomenti
+valid_field(field(FieldName, Value, Type)) :-
+    atom(FieldName),
+    ground(Value), % Verifica se il valore è istanziato
+    (   var(Type)
+    ;   atom(Type)
+    ),
+    !. % Utilizzo del cut per evitare backtracking indesiderato
+
+% Se la struttura non è conforme ai due metodi precedenti, la regola fallisce.
+valid_field(Field) :-
+    format("Errore: Il campo ~w non è valido.~n", [Field]),
+    fail.
+
+% Validazione di un metodo
+valid_method(method(MethodName, ArgList, Form)) :-
+    atom(MethodName),
+    is_prolog_list(ArgList),
+    is_prolog_form(Form),
+    !. % Utilizzo del cut per evitare backtracking indesiderato
+
+% Se la struttura non è conforme a method/3, la regola fallisce
+valid_method(Method) :-
+    format("Errore: Il metodo ~w non è valido.~n", [Method]),
+    fail.
+
+% Verifica se un termine è una congiunzione di predicati Prolog
+is_prolog_form(Form) :-
+    is_list(Form),
+    maplist(prolog_predicate, Form).
+
+% Verifica se un termine è un predicato Prolog
+prolog_predicate(Predicate) :-
+    functor(Predicate, Functor, _),
+    atom(Functor).
+
+
 
 
 
